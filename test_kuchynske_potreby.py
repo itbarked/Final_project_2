8102ac3c058822e2d5ba61842bad4d15ff9db5bf
itbarked <stepanka.kuc@seznam.cz>
2025-03-03 19:46:16 +0100

    skoro final (nefachá kontrola řazení items)

--- test_kuchynske_potreby.py
+++ test_kuchynske_potreby.py
@@ -5,15 +5,15 @@ from playwright.sync_api import sync_playwright
 @pytest.fixture()
 def browser():
     with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=True, slow_mo=1000)
+        browser = playwright.chromium.launch(headless=False, slow_mo=1500)
         yield browser
         browser.close()
 
-#@pytest.fixture()
-#def page(browser):
-#    page=browser.new_page()
-#    yield page
-#    page.close()
+@pytest.fixture()
+def page(browser):
+    page=browser.new_page()
+    yield page
+    page.close()
 
 def test_kuchynske_potreby(page):
     url="https://www.kuchynskepotreby.cz/"
@@ -31,5 +31,22 @@ def test_kuchynske_potreby(page):
     znacka_orion_checkbox = page.locator("#maklab57")
     znacka_orion_checkbox.check()
     
+    page.wait_for_selector(".item", state="visible", timeout=10000)
+
     nejlevnejsi_button = page.locator("#li_price")
+    page.wait_for_selector("#li_price", state="visible", timeout=10000)
     nejlevnejsi_button.click()
+
+    page.wait_for_selector(".item", state="visible", timeout=10000)
+
+    #kontrola, že jsou ceny skutečně srovnány od nejnižší po nejvyšší
+    prices = page.query_selector_all(".price")
+    price_values = []
+    
+    for price in prices:
+        price_text = price.inner_text()
+        price_text = price_text.replace(" Kč", "").replace("0\nPřejít k objednávce", "0").strip()
+        price_number = float(price_text.replace(",", "."))
+        price_values.append(price_number)  
+        print(price_values)
+        assert price_values == sorted(price_values), "Ceny nejsou seřazeny vzestupně."
