51fe47c5989693fe32806c14659bfaf775ed14b4
itbarked <stepanka.kuc@seznam.cz>
2025-03-11 22:05:56 +0100

    final

--- test_kuchynske_potreby_2.py
+++ test_kuchynske_potreby_2.py
@@ -16,24 +16,24 @@ def setup_browser(request):
 
 @pytest.mark.parametrize("username, password, expected_message",
     [
-        ("stepanka.kuc@seznam.cz", "engeto", ""), #všechno správně
         ("c@seznam.cz", "geto", "Zadaná e-mailová adresa není zaregistrována nebo není aktivována!"), #všechno špatně
         ("stepanka.kuc@seznam.cz", "geto", "Zadal(a) jste špatně přihlašovací e-mail nebo heslo."), #správné username, špatně password
         ("stepanka.kuc@seznam.cz", "", ""), #správné username, prázdné password
         ("c@seznam.cz", "", ""), #špatně username, prázdné password
         ("", "", ""), #všechno prázdné
+        ("stepanka.kuc@seznam.cz", "engeto", ""), #všechno správně
     ]
 )
 def test_login(setup_browser, username, password, expected_message):
     page = setup_browser
     page.goto("https://www.kuchynskepotreby.cz/")
-
+    
     prihlaseni_button = page.locator("#header > div > div > div > div.col.account > div > a:nth-child(3)")
     prihlaseni_button.click()
 
-    page.fill('input[name="E-mail"]', username)
-    page.fill('input[name="Heslo"]', password)
-    page.click('button[type="submit"]')
+    page.fill("#lemail", username)
+    page.fill("input[type=password]", password)
+    page.click(".submit")
 
     page.wait_for_selector(f'text={expected_message}')
 
